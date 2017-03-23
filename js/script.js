124181d309c8c8fb95be23ee521119ac8593733b
zakir3 <zakir@Zakirs-MBP.home>
2017-03-23 18:44:38 +0000

    Accordion
    Accordion boxes closer together.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-  $(function () {
+
+
+
+
+
+
+
+$(function () {
       $("#language-toggle").click(function () {
           $(".text-on").switchClass("text-on", "text-off", 10);
           $(".text-off").switchClass("text-off", "text-on", 10);
@@ -15,4 +22,3 @@
           $('nav').removeClass('fixed-header');
       }
   });
-
