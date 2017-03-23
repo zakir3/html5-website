153a59b7a95dfe579274ec0326b5455c0e4861a0
zakir3 <zakir@Zakirs-MBP.home>
2017-03-23 17:09:44 +0000

    Amendments to Nav bar
    Nav bar is now fixed at the top and header image disappears.

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@
   });
 
 
+
+  $(window).scroll(function(){
+      if ($(window).scrollTop() >= 300) {
+          $('nav').addClass('fixed-header');
+      }
+      else {
+          $('nav').removeClass('fixed-header');
+      }
+  });
+
