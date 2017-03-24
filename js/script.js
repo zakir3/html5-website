58b196590f5d0c9a1b763127b9da439b16651b5c
zakir3 <zakir@Zakirs-MBP.home>
2017-03-24 02:35:07 +0000

    Added modal bar
    When user clicks on a book in the accordion, a modal bar appears with
    more info. Working for 1 book as a trial.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-
-
-
-
-
-
-
 $(function () {
       $("#language-toggle").click(function () {
           $(".text-on").switchClass("text-on", "text-off", 10);
@@ -22,3 +15,37 @@ $(function () {
           $('nav').removeClass('fixed-header');
       }
   });
+
+$(function () {
+
+
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the button that opens the modal
+    var img = document.getElementById("image1");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    img.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+
+});
+
+
