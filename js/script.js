6b86790165b7c6e859b11249fd1375a14a4f9377
zakir3 <zakir@Zakirs-MBP.home>
2017-03-24 07:41:01 +0000

    Added Gallery + Contact
    Gallery with images of library added to gallery section. Contact us
    form now working

--- js/script.js
+++ js/script.js
@@ -222,21 +222,13 @@ for (i = 0; i < acc.length; i++) {
 
 
 $(function () {
-
 $('.btn-english').click(function () {
     $('.arabic').hide();
     $('.english').show();
-
-
 });
-
 $('.btn-arabic').click(function () {
     $('.english').hide();
     $('.arabic').show();
 });
-
-
 });
-
-
 
